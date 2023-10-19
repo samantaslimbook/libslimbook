c7b247c88e1f0f7902e7a840136d8fd8bb064e85
Lt-Henry <quiqueiii@gmail.com>
2023-10-19 22:00:14 +0200

    * added lgpl license * added serial number get function

--- src/slimbook.cpp
+++ src/slimbook.cpp
@@ -1,3 +1,22 @@
+/*
+Copyright (C) 2023 Slimbook <dev@slimbook.es>
+
+This file is part of libslimbook.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 
 #include "slimbook.h"
 
@@ -21,7 +40,7 @@ static void read_device(string path,string& out)
     file.close();
 }
 
-const char* slb_product_name()
+const char* slb_info_product_name()
 {
     buffer.clear();
 
@@ -30,11 +49,20 @@ const char* slb_product_name()
     return buffer.c_str();
 }
 
-const char* slb_board_vendor()
+const char* slb_info_board_vendor()
 {
     buffer.clear();
 
     read_device(SYSFS_DMI"board_vendor",buffer);
+
+    return buffer.c_str();
+}
+
+const char* slb_info_serial_number()
+{
+    buffer.clear();
+
+    read_device(SYSFS_DMI"serial_number",buffer);
 
     return buffer.c_str();
 }
--- src/slimbookctl.cpp
+++ src/slimbookctl.cpp
@@ -1,3 +1,22 @@
+/*
+Copyright (C) 2023 Slimbook <dev@slimbook.es>
+
+This file is part of libslimbook.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 
 #include "slimbook.h"
 
@@ -7,7 +26,7 @@ using namespace std;
 
 int main(int argc,char* argv[])
 {
-    cout<<"product:["<<slb_product_name()<<"]"<<endl;
-    cout<<"vendor:["<<slb_board_vendor()<<"]"<<endl;
+    cout<<"product:["<<slb_info_product_name()<<"]"<<endl;
+    cout<<"vendor:["<<slb_info_board_vendor()<<"]"<<endl;
     return 0;
 }
